33d78e0187e27f91c9951f0f64a26063fd885f7b
Naina Sahrawat <nsahra14@gmail.com>
2016-09-06 15:53:51 -0400

    response from API fills in html table

--- static/bidpredictor.js
+++ static/bidpredictor.js
@@ -28,7 +28,9 @@ function BidInfo() {
 	var std = document.getElementById("std_dev_field").value;
 	var opening = document.getElementById("open_field").value;
 	
-	var request = gapi.client.ebaybidsendpoints.calculateClosing({'opening': opening, 'average': average, 'std': std});
+	var request = gapi.client.ebaybidsendpoints.calculateClosing(
+		{'opening': opening, 'average': average, 'std': std}
+	);
 	
 	request.execute(BidInfoCallback);
 
@@ -36,6 +38,19 @@ function BidInfo() {
 
 function BidInfoCallback(response) {
 
-	alert(response.msg);
+	//alert("response = " + response.msg);
+	
+	var table = document.getElementById("rslt_tbl");
+
+	var average = document.getElementById("average_field").value;
+	var std = document.getElementById("std_dev_field").value;
+
+	table.rows[0].cells[1].innerHTML = "$" + average;
+	table.rows[1].cells[1].innerHTML = "$" + std;
+	table.rows[2].cells[1].innerHTML = "$" + response.opening;
+	table.rows[3].cells[1].innerHTML = "$" + response.delta;
+	table.rows[4].cells[1].innerHTML = "$" + response.closing;
+
 
 }
+
